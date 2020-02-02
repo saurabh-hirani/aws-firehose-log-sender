71166e05c28036228e206b7a522d3ee9bbbd0ac0
Saurabh Hirani <saurabh.hirani@gmail.com>
2020-02-02 21:39:30 +0800

    simplifying lambda call wrapper

--- test-lambda.sh
+++ test-lambda.sh
@@ -1,17 +1,6 @@
 #!/bin/bash -e
 
-usage="$0 src_account_number target_account_number"
-
-if [[ $# -ne 2 ]]; then
-  echo "ERROR: Invalid usage"
-  echo "Usage: $usage"
-  exit 1
-fi
-
-src_account_number=$1
-target_account_number=$2
-
-aws lambda invoke --function-name test_firehose_logger --payload "$(cat test-lambda-$src_account_number-to-$target_account_number-payload.json)" test-lambda-response.json
+aws lambda invoke --function-name test-firehose-log-sender-lambda --payload "$(cat $1)" test-lambda-response.json
 echo
 echo "Lambda function returned $(cat test-lambda-response.json)"
 rm test-lambda-response.json
